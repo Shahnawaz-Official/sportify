07f0aa4aa2f8ea8fe76adf892e058e1d3d485755
shahnawaz <mdshahnawaz.420bca@gmail.com>
2026-02-27 15:32:59 +0530

    database connected

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const app = require("./src/app")
+const PORT  = process.env.PORT || 3000;
+const connectDB = require("./src/db/db")
+
+
+
+
+connectDB();
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running PORT ${PORT}`);
+    
+})
